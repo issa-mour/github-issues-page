c2ca8ec15af4bfff502d314fd7096fa75d08831f
issa-mour <issa.mour2@gmail.com>
2018-09-12 15:47:15 +0300

    paging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 import IssuesList from './components/issues/IssuesList';
+import Paging from './components/paging/Paging';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<IssuesList />
+			<React.Fragment>
+				<IssuesList />
+				<Paging />
+			</React.Fragment>
 		);
 	}
 }
