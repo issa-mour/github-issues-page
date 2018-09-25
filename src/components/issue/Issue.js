77d03f75ec42b531e5ecf679157fa059bcda1e61
issa-mour <issa.mour2@gmail.com>
2018-09-25 14:43:15 +0300

    createdAt is Date

--- src/components/issue/Issue.js
+++ src/components/issue/Issue.js
@@ -10,16 +10,15 @@ Issue.propTypes = {
 	className: PropTypes.string,
 	login: PropTypes.string.isRequired,
 	body: PropTypes.string.isRequired,
-	createdAt: PropTypes.string.isRequired
+	createdAt: PropTypes.instanceOf(Date).isRequired
 };
 
 function Issue({ className, login, body, createdAt }) {
 	const cls = classNames('issue', className);
-	const date = new Date(createdAt);
 	return <IntlRelativeFormatContext.Consumer>
 		{rf => (<div className={cls}>
 			<div className="issue_header">
-				<a className="issue_header_login" href={`/${login}`}>{login}</a> commented <span title={date}>{rf.format(date)}</span>
+				<a className="issue_header_login" href={`/${login}`}>{login}</a> commented <span title={createdAt}>{rf.format(createdAt)}</span>
 			</div>
 			<ReactMarkdown className="issue_header_body" source={body} />
 		</div>)}
