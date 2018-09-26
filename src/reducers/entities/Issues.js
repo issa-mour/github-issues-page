974036fdb044be68e535a3b0dca529d7c545fc0c
issa-mour <issa.mour2@gmail.com>
2018-09-26 12:31:02 +0300

    fix race condition on issue page

--- src/reducers/entities/Issues.js
+++ src/reducers/entities/Issues.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
-import { GET_ISSUES_SUCCESS, GET_COMMENTS_SUCCESS, GET_ISSUE_SUCCESS } from '../../actions/index';
+import { GET_ISSUES_SUCCESS, GET_ISSUE_SUCCESS } from '../../actions/index';
 const INITIAL_STATE = fromJS({ byId: {}, allIds: [] });
 
 function mergeIssues(state, payload) {
@@ -8,12 +8,6 @@ function mergeIssues(state, payload) {
 	const issues = p.getIn(['issues', 'entities', 'issues'], fromJS({}));
 	const allIds = p.getIn(['issues', 'result'], fromJS([]));
 	return state.update('byId', () => issues).update('allIds', () => allIds);
-}
-
-function updateCommentsIds(state, payload) {
-	const p = fromJS(payload);
-	const g = p.getIn(['comments', 'entities', 'comments'], fromJS({})).valueSeq().map(comment => comment.get('id'));
-	return state.updateIn(['byId', payload.issueId.toString(), 'commentsIds'], () => g);
 }
 
 function mergeIssue(state, payload) {
@@ -28,8 +22,6 @@ export default function (state = INITIAL_STATE, { type, payload }) {
 			return mergeIssues(state, payload);
 		case GET_ISSUE_SUCCESS:
 			return mergeIssue(state, payload);
-		case GET_COMMENTS_SUCCESS:
-			return updateCommentsIds(state, payload);
 		default:
 			return state;
 	}
