864cf49ddbb0be4667438b6d919cb761708bb77e
issa-mour <issa.mour2@gmail.com>
2018-09-28 13:08:20 +0300

    split code routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import IssuesPage from './pages/issues/IssuesPage';
-import IssuePage from './pages/issue/IssuePage';
+import asyncComponent from './components/AsyncComponent';
 import ScrollToTop from './components/ScrollToTop';
-import NewIssueContainer from './containers/issue/NewIssueContainer';
 import IntlRelativeFormatContext, { intlRelativeFormat } from './IntlRelativeFormatContext';
 
 import './App.css';
+
+const IssuesPage = asyncComponent(() => import(/* webpackChunkName: "issues_list" */'./pages/issues/IssuesPage'));
+const IssuePage = asyncComponent(() => import(/* webpackChunkName: "issue" */'./pages/issue/IssuePage')); 
+const NewIssueContainer = asyncComponent(() => import(/* webpackChunkName: "new_issue" */'./containers/issue/NewIssueContainer'));
 
 class App extends Component {
 	render() {
